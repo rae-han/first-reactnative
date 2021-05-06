15cdb87edc956f7e2bb1f489cce972195c5a47a0
Raehan <raehan900809@gmail.com>
2021-05-06 21:28:42 +0900

    Add icon

--- react-native-todo/src/App.js
+++ react-native-todo/src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'react-native'
 import styled, { ThemeProvider } from 'styled-components/native'
 import { theme } from './theme'
+
+import Input from './components/Input'
 
 const Container = styled.SafeAreaView`
   flex: 1;
@@ -19,6 +21,17 @@ const Title = styled.Text`
 `
 
 const App = () => {
+  const [newTask, setNewTask] = useState('');
+
+  const _addTask = () => {
+    alert(`Add: ${newTask}`);
+    setNewTask('');
+  }
+
+  const _handleTextChange = text => {
+    setNewTask(text)
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Container>
@@ -27,7 +40,12 @@ const App = () => {
           backgroundColor={theme.background}
         />
         <Title>TODO List</Title>
-
+        <Input 
+          placeholder="+ Add a Task" 
+          value={newTask}
+          onChangeText={_handleTextChange}
+          onSubmitEditing={_addTask}
+        />
       </Container>
     </ThemeProvider>
   );
